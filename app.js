a39abc3b18ceec335d0be56b7d0d9b7ee2e759c5
Kevin To <kevinto1989@gmail.com>
2016-09-28 01:24:37 +0000

    added sample endpoints.

--- app.js
+++ app.js
@@ -8,15 +8,29 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var errorhandler = require('errorhandler');
-
+var mongoose = require('mongoose');
 var app = express();
+var bodyParser = require('body-parser')
+
+// Setup db
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/todo-api')
+  .then(() =>  console.log('Mongodb connection successful'))
+  .catch((err) => console.error(err));
+
+// This needs to be above any routing in order to get the body correctly
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+ 
+// Sample TODO
+var todos = require('./routes/todos');
+app.use('/todos', todos);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 // app.use(express.favicon());
 // app.use(express.logger('dev'));
-// app.use(express.bodyParser());
 // app.use(express.methodOverride());
 // app.use(app.router);
 // app.use(express.static(path.join(__dirname, '/public/views')));
@@ -32,6 +46,7 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
+
 
 var port = process.env.PORT || 3000;
 var hostName = process.env.IP || "0.0.0.0";
