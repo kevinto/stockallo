7d0b78baf598c5fd6287f8c79434812312a8f7af
Kevin To <kevinto1989@gmail.com>
2016-09-28 01:54:45 +0000

    added dynamic viewing structure

--- app.js
+++ app.js
@@ -5,12 +5,13 @@
 
 var express = require('express');
 // var routes = require('./routes');
-var http = require('http');
 var path = require('path');
 var errorhandler = require('errorhandler');
 var mongoose = require('mongoose');
-var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 
 // Setup db
 mongoose.Promise = global.Promise;
@@ -18,13 +19,21 @@ mongoose.connect('mongodb://localhost/todo-api')
   .then(() =>  console.log('Mongodb connection successful'))
   .catch((err) => console.error(err));
 
+var app = express();  
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // This needs to be above any routing in order to get the body correctly
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
- 
+
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Sample TODO
 var todos = require('./routes/todos');
-app.use('/todos', todos);
+var routes = require('./routes/index');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -41,12 +50,8 @@ if ('development' == app.get('env')) {
   app.use(errorhandler());
 }
 
-// app.get('/', routes.index);
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
+app.use('/', routes);
+app.use('/todos', todos);
 
 var port = process.env.PORT || 3000;
 var hostName = process.env.IP || "0.0.0.0";
